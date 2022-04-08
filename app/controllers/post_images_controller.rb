74c0aec949538c83677ea1a743ae98ebdbe8b97d
EC2 Default User <ec2-user@ip-172-31-89-77.ec2.internal>
2022-04-08 01:46:27 +0000

    [Add] chapter13

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -26,6 +26,12 @@ class PostImagesController < ApplicationController
   def edit
   end
   
+  def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path
+ end
+  
   # 投稿データのストロングパラメータ
   private
 
