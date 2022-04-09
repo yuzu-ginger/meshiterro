51ed7942737e4e03e983ba794fe47b954d188d3d
EC2 Default User <ec2-user@ip-172-31-89-77.ec2.internal>
2022-04-09 20:08:55 +0000

    [Add] chapter19

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
   devise_for :users
   
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
   # get 'post_image/new'
